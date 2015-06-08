5cfa910f6ee0c9e148c13f6241d8da9102318886
augstm <maxime.augst@iut-valence.fr>
2015-06-08 16:14:19 +0200

    add a class

---
+++ projet/src/javaproject_ageOfAntHill/entity/buildable/AntHill.java
@@ -0,0 +1,12 @@
+package javaproject_ageOfAntHill.entity.buildable;
+
+public class AntHill extends Building {
+	/**
+	  * Just like a serial name, it's used to identify an anthill's type
+	  */
+	 private final static String ANTHILL_TYPE = "AHILL";
+	 
+	/* public AntHill(){
+		 this.setType(ANTHILL_TYPE);
+	 }*/
+}
