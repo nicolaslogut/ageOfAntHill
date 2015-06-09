4b5245c2293d4b6578395c443230b4e70f075faf
adrienIutValence <arsac.adrien@iut-valence.fr>
2015-06-09 16:34:39 +0200

    changes few classes

--- projet/src/javaproject_ageOfAntHill/entity/buildable/House.java
+++ projet/src/javaproject_ageOfAntHill/entity/buildable/House.java
@@ -1,5 +1,14 @@
 package javaproject_ageOfAntHill.entity.buildable;
 
 public class House extends Building {
-
+	/**
+	  * Just like a serial name, it's used to identify an House's type
+	  */
+	 private final static String HOUSE_TYPE = "HOUSE";
+	 
+	 private final static int HOUSE_HP = 250;
+	 
+	public House(){
+		 this.setType(HOUSE_TYPE);
+	 }
 }
--- projet/src/javaproject_ageOfAntHill/entity/buildable/PostGuard.java
+++ projet/src/javaproject_ageOfAntHill/entity/buildable/PostGuard.java
@@ -2,5 +2,14 @@ package javaproject_ageOfAntHill.entity.buildable;
 
 
 public class PostGuard extends Building{
-
+	/**
+	  * Just like a serial name, it's used to identify an PostGuard's type
+	  */
+	 private final static String POSTGUARD_TYPE = "POSTG";
+	 
+	 private final static int POSTGUARD_HP = 500;
+	 
+	public PostGuard(){
+		 this.setType(POSTGUARD_TYPE);
+	 }
 }
